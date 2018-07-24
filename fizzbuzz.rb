76456e73bf22049b9ddb5f5e3ce7285dbcd1a883
hiromi0226 <hiromi.26ha@gmail.com>
2018-07-24 18:16:37 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+
+def fizzBuzz (num)
+  if num % 5 == 0 && num % 3 == 0
+     puts "FizzBuzz"
+  elsif num % 3 == 0 
+    puts "Fizz"
+  elsif num % 5 == 0 
+    puts "Buzz"
+  else 
+    puts "No Answer"
+  end 
+end 
+
+fizzBuzz(15)
---
+++ style.rb
@@ -0,0 +1,4 @@
+loop do 
+ puts "to the right to the right to the right to the right"
+ puts "to the left to the left to the left"
+ end 
