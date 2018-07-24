f4d9d8fe11f9e8cc252d8c70d090b9c6b2c1e806
hiromi0226 <hiromi.26ha@gmail.com>
2018-07-24 18:29:56 +0000

    automatically backed up by learn

--- style.rb
+++ style.rb
@@ -5,6 +5,10 @@ while count < 10
 puts "to the right to the right to the right"
 puts "to the left to the left to the left"
 
-count = count + 1 
+count += 1 
 
-puts "#"
+puts "#{count} move(s) done!"
+end
+end 
+
+cupid shuffle 
