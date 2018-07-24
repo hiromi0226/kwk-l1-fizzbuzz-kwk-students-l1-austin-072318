3b5fae3408463a62362d93e7ca695e03e6a2fd3f
hiromi0226 <hiromi.26ha@gmail.com>
2018-07-24 18:28:05 +0000

    automatically backed up by learn

--- style.rb
+++ style.rb
@@ -1,16 +1,10 @@
 def cupid_shuffle
   count = 0 
-loop do 
-  if count == 13
- break 
- end 
-  puts "to the right to the right to the right to the right"
-  puts "to the left to the left to the left to the left to the left"
-  
-  count = count + 1 
-  
-  puts "#{count} move(s) completed!"
- end
- end 
- 
- cupid_shuffle
+
+while count < 10 
+puts "to the right to the right to the right"
+puts "to the left to the left to the left"
+
+count = count + 1 
+
+puts "#"
